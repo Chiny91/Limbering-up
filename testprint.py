9834637c6b73630f4b6c5b112bf380a474dec16f
Chiny91 <github@goldhill.co.uk>
2017-07-24 18:52:29 +0100

    Coloured text

--- testprint.py
+++ testprint.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python
 
-# Import some modules, use pip install
+# Import some module/s
 import curses
-import time
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
+def main(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
+    try:
+        for i in range(0, 255):
+            stdscr.addstr(str(i), curses.color_pair(i))
+        stdscr.addstr(7, 0, "Hello World", curses.color_pair(1))
+        stdscr.addstr(8, 0, "Red text", curses.color_pair(2))
+        stdscr.addstr(9, 0, "GREEN ALERT!", curses.color_pair(3))
+        stdscr.addstr(10, 0, "Boris is the biggest English traitor since Tostig Godwinson", curses.color_pair(7))
+        stdscr.addstr(12, 0, "Hit any key to end program")
+        stdscr.refresh()
 
-stdscr.addstr("Hello World")
-stdscr.refresh()
+    except curses.ERR:
+        # End of screen reached
+        pass
 
-try:
-    while True:
-        time.sleep(0.001)
-except KeyboardInterrupt:
-    pass
+     #  Pause and wait for key press
+    stdscr.getch()
 
-curses.nocbreak()
-curses.echo()
-curses.endwin()
+# Gracefully exit curses and tidy up terminal
+curses.wrapper(main)
 
-# Python appears to need a line with same indent as the program start
+# Standard program end with same indent as the program start
 raise SystemExit
